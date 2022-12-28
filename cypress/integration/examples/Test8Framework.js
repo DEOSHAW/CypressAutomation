f19ea41ea93006e87af1ecd8ebc93bc6b4da72b3
Deo Prasad Shaw <deoshaw3@gmail.com>
2022-12-28 14:50:15 +0530

    Modified test file

--- cypress/integration/examples/Test8Framework.js
+++ cypress/integration/examples/Test8Framework.js
@@ -58,7 +58,7 @@ describe('My Second Test Suite', function () {
     cy.get('.suggestions > ul > li > a').click()
     cy.get('#checkbox2').click({ force: true })
     cy.get('input[type="submit"]').click()
-    //cy.get('.alert').should('have.text','Success! Thank you! Your order will be delivered in next few weeks :-).')
+    //cy.get('.alert').should('have.text','Success! Thank you! Your order will be delivered in next few weeks :-)
     cy.get('.alert').then(function (element) {
       const actualText = element.text()
       expect(actualText.includes("Success")).to.be.true
