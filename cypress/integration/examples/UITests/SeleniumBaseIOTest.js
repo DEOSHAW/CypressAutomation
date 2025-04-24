a752ebf89988c8f0a7ca718612c3c04825a7d1e8
Deo Prasad Shaw <deoshaw3@gmail.com>
2025-04-24 09:36:23 +0530

    Added SeleniumBaseIO hover test

--- cypress/integration/examples/UITests/SeleniumBaseIOTest.js
+++ cypress/integration/examples/UITests/SeleniumBaseIOTest.js
@@ -1,6 +1,9 @@
 describe('Test Suite',()=>{
+    beforeEach('Launch Portal',()=>{
+        cy.visit('https://seleniumbase.io/demo_page');
+    })
     it.skip('Validate button colour',()=>{
-        cy.visit('https://seleniumbase.io/demo_page/');
+        
         cy.get('button#myButton').should('have.attr','style','color: green;');
         cy.get('button#myButton').click();
         cy.get('button#myButton').should('have.attr','style','color: purple;');
@@ -9,12 +12,21 @@ describe('Test Suite',()=>{
 
     it.skip('Type in prefilled text',()=>
     {
-        cy.visit('https://seleniumbase.io/demo_page/');
         cy.get('input#myTextInput2').clear().type('Test Data');
     })
 
-    it('Drodown Test',()=>{
-        cy.visit('https://seleniumbase.io/demo_page');
+    it.skip('Drodown Test',()=>{
         cy.get('#mySelect').select(3);
+    })
+
+    it('Hover Test',()=>{
+        cy.get('div#myDropdown').invoke('show');
+        cy.get('div.dropdown-content>a').should('have.length',3).each((el,index,list)=>
+        {
+           cy.log(el.text());
+        })
+        cy.get('#dropOption3').should('have.text','Link Three');
+
+
     })
 })
