9eab0d27e612ad60c15ce7b1f1563f2ab09b276d
Deo Prasad Shaw <deoshaw3@gmail.com>
2025-07-04 10:22:10 +0530

    Added Heroku upload test

--- cypress/integration/examples/UITests/HerokuTests.js
+++ cypress/integration/examples/UITests/HerokuTests.js
@@ -1,25 +1,28 @@
-describe('Heroku Test suite',function(){
+import 'cypress-file-upload';
+describe('Heroku Test suite', function () {
 
-    it('Select from Dropdown',()=>{
-            cy.visit('https://the-internet.herokuapp.com/dropdown');
-            cy.get('select#dropdown').select('Option 1');
-            cy.get('select#dropdown');
-            cy.get('h3').should('have.text','Dropdown List');
+    it('Upload File Test', () => {
+            cy.visit('https://the-internet.herokuapp.com/upload');
+            cy.get('input#file-upload').attachFile('Test1.txt');
+            cy.get('input#file-submit').click();
+            cy.get('div#uploaded-files').should('contain.text','Test1.txt');
         })
 
- 
-    it('Test-Dynamic element loading',()=>{
+    it.skip('Select from Dropdown', () => {
+        cy.visit('https://the-internet.herokuapp.com/dropdown');
+        cy.get('select#dropdown').select('Option 1');
+        cy.get('select#dropdown');
+        cy.get('h3').should('have.text', 'Dropdown List');
+    })
+
+
+    it.skip('Test-Dynamic element loading', () => {
         cy.visit('https://the-internet.herokuapp.com/dynamic_loading');
         cy.get("a[href='/dynamic_loading/2']").click();
         cy.get('#start>button').click();
         cy.get('#finish>h4').should('be.visible').
-        then(function(ele){
-            expect(ele.text()).to.contain('Hello World!');
-
-
+            then(function (ele) {
+                expect(ele.text()).to.contain('Hello World!');
+            })
         })
-
-        
-        
-    })
 })
