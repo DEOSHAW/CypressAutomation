7142715a401b399e65ab608853d492c04c17847d
Deo Prasad Shaw <deoshaw3@gmail.com>
2025-10-16 10:04:48 +0530

    Demo QA Frame test

--- cypress/integration/examples/UITests/DemoQATest.js
+++ cypress/integration/examples/UITests/DemoQATest.js
@@ -1,9 +1,15 @@
 import 'cypress-drag-drop';
+import 'cypress-iframe';
 describe('Test Suite',()=>{
-    it('Drag and Drop test',()=>{
+    it.skip('Drag and Drop test',()=>{
         const dataTransfer = new DataTransfer();
         cy.visit('https://demoqa.com/droppable');
         //cy.get('div#draggable').drag('div#simpleDropContainer>div#droppable');
         cy.get('div#simpleDropContainer>div#droppable>p').should('have.text','Drop here');
     })
+
+    it('Get text inside frame',()=>{
+        cy.visit('https://demoqa.com/frames');
+        cy.iframe('#frame1').find('h1#sampleHeading').should('have.text','This is a sample page');
+})
 })
