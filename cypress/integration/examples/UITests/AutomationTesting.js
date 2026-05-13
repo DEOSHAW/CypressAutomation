1e2ef10bc5ca701a7a07d36ce960c63082127a7e
Deo Prasad Shaw <deoshaw3@gmail.com>
2026-05-13 10:52:57 +0530

    Added HoldShift Click test

--- cypress/integration/examples/UITests/AutomationTesting.js
+++ cypress/integration/examples/UITests/AutomationTesting.js
@@ -1,9 +1,18 @@
 describe('Test Suite',()=>{
-    it('Dropdown Selection Test',()=>
+    it.skip('Dropdown Selection Test',()=>
     {
       cy.visit('https://www.automationtesting.co.uk/dropdown.html');
       cy.get('select#cars').select('Mercedes')
       .find('option:selected')
       .should('have.text', 'Mercedes');
     })
+
+    it('Hold Shift and Click Test',()=>
+    {
+      cy.visit('https://www.automationtesting.co.uk/actions.html');
+      cy.xpath("(//div[@id='doubleClickArea'])[2]").click({shiftKey: true});
+      cy.on('window:alert', (text) => {
+      expect(text).to.equal('The SHIFT key was pressed!')
+})
+    });
 })
