6f7a85276d18ddb4b2e94219e63bd7c87c3995d8
Deo Prasad Shaw <deoshaw3@gmail.com>
2026-06-11 08:54:35 +0530

    Added Automation testing print table test

--- cypress/integration/examples/UITests/AutomationTesting.js
+++ cypress/integration/examples/UITests/AutomationTesting.js
@@ -16,11 +16,31 @@ describe('Test Suite', () => {
     })
   });
 
-  it('Upload File Test', () => {
+  it.skip('Upload File Test', () => {
     const fileName = Cypress.env('uploadFile') || 'Test1.txt';
 
     cy.visit('https://www.automationtesting.co.uk/fileupload.html');
     cy.get('input[type="file"]').attachFile(fileName);
     cy.get("input[type='submit']").click();
   });
-})
+
+  it('Print Table Test',()=>{
+    cy.visit('https://www.automationtesting.co.uk/tables.html');
+    cy.xpath("//table//tbody//tr").then((rows)=>{
+      const rowCount = rows.length;
+      cy.log(`Total number of rows: ${rowCount}`);
+    })
+
+    cy.xpath("//table//tbody//tr[1]//td").then((cells)=>{
+      const cellCount = cells.length;
+      cy.log(`Total number of cells in the first row: ${cellCount}`);
+    })
+
+    cy.xpath("//table//tbody//tr").each(($row, index) => {
+      cy.wrap($row).find('td').each(($cell, cellIndex) => {
+        const cellText = $cell.text();
+        cy.log(`Row ${index + 1}, Cell ${cellIndex + 1}: ${cellText}`);
+      });
+    });
+  });
+});
